811ce1183c3e588145e1652b9dbd16ea58e73031
Haoran Zhang <31876749+Zhang-Haoran@users.noreply.github.com>
2021-06-25 10:28:15 +1000

    add auth testing

--- back-end/Auth/src/controller/auth.js
+++ back-end/Auth/src/controller/auth.js
@@ -1,22 +1,25 @@
 const {generateToken} = require("../utils/auth");
 const User = require("../model/user");
 
+//user login
 async function login(req,res){
+    //check if username or password is empty
     if (req.body.username === undefined ||req.body.password === undefined ){
         return res.status(400).send({error:"username or password is not defined"})
     }
     const {username, password} = req.body;
 
+    //check if the user is register in database
     const user = await User.findOne({username});
     if (!user){
         return res.status(404).json({error:"username not found"});
     }
 
+    //check if the password is correct
     const validatePassword = await user.validatePassword(password);
     if (!validatePassword){
         return res.status(401).json({error:"invalid password"});
     }
-
 
     return res.status(200).json({token:generateToken({id:user._id}),username})
 }
--- back-end/Auth/src/tests/auth.test.js
+++ back-end/Auth/src/tests/auth.test.js
@@ -3,15 +3,35 @@ const app = require("../app");
 
 const request = supertest(app);
 
-it("should return token if request contain valid username and password", async function () {
-  const res = await request
-    .post("/api/v1/auth")
-    .send({ username: "Operation", password: "123" });
-  expect(res.statusCode).toBe(200);
-  expect(Object.keys(res.body)).toContain("token");
-});
+describe("/auth",()=>{
+  describe("POST",() => {
+    it("should return token if request contain valid username and password", async function () {
+      const res = await request
+          .post("/api/v1/auth")
+          .send({ username: "Operation", password: "123" });
+      expect(res.statusCode).toBe(200);
+      expect(Object.keys(res.body)).toContain("token");
+    });
 
-it("should return error message if request body does not contain username or password", async function () {
-  const res = await request.post("/api/v1/auth").send({});
-  expect(res.statusCode).toBe(400);
-});
+    it("should return error message if request body does not contain username or password", async function () {
+      const res = await request.post("/api/v1/auth").send({});
+      expect(res.statusCode).toBe(400);
+    });
+
+    it('should return error message if username does not find in database', async function () {
+      const res = await request.post("/api/v1/auth").send({username:"Admin",password:"123"});
+      expect(res.statusCode).toBe(404);
+    });
+
+    it('should return error message if password does not match username in database', async function () {
+      const res = await request.post("/api/v1/auth").send({username:"Operation",password:"111"});
+      expect(res.statusCode).toBe(401);
+    });
+  })
+})
+
+
+
+
+
+
