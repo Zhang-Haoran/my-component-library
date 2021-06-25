177423f435700143caceb0143622119273b616fd
Haoran Zhang <31876749+Zhang-Haoran@users.noreply.github.com>
2021-06-25 10:50:00 +1000

    auth testing updated

--- back-end/Auth/src/tests/auth.test.js
+++ back-end/Auth/src/tests/auth.test.js
@@ -11,6 +11,7 @@ describe("/auth",()=>{
           .send({ username: "Operation", password: "123" });
       expect(res.statusCode).toBe(200);
       expect(Object.keys(res.body)).toContain("token");
+      expect(Object.keys(res.body)).toContain("username");
     });
 
     it("should return error message if request body does not contain username or password", async function () {
