574aeeaf96a00d5bf4c10a448a4254baf064228a
Haoran Zhang <31876749+Zhang-Haoran@users.noreply.github.com>
2021-07-05 10:33:08 +1000

    components init

---
+++ frontend/src/components/BestChoice/styles/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.section``;
+export const Title = styled.h2``;
+export const Subtitle = styled.h3``;
+export const Paragraph = styled.p``;
+export const Left = styled.div``;
+export const Right = styled.div``;
