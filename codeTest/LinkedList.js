54d4db1291535e968eff0cae3de87f98ae71d8f7
Haoran Zhang <31876749+Zhang-Haoran@users.noreply.github.com>
2021-07-05 16:15:32 +1000

    LinkedList

--- codeTest/LinkedList.js
+++ codeTest/LinkedList.js
@@ -4,8 +4,10 @@ class ListNode{
         this.next = next === undefined? null: next;
     }
 }
-const listNode = new ListNode(1);
-const listNode2 = new ListNode(2,listNode);
-const listNode3 = new ListNode(3,listNode2);
-const listNode4 = new ListNode(4,listNode3);
-const listNode5 = new ListNode(5,listNode4);
+const listNode5 = new ListNode(5);
+const listNode4 = new ListNode(4,listNode5);
+const listNode3 = new ListNode(3,listNode4);
+const listNode2 = new ListNode(2,listNode3);
+const listNode1 = new ListNode(1,listNode2);
+
+console.log(listNode1);
