d1e4e35bb98e237dbbac9360895e9cf1fc2534eb
Haoran Zhang <31876749+Zhang-Haoran@users.noreply.github.com>
2021-07-06 11:55:40 +1000

    adjust css for frontend

--- frontend/src/components/BestChoice/styles/index.js
+++ frontend/src/components/BestChoice/styles/index.js
@@ -6,7 +6,7 @@ export const Container = styled.section`
 `;
 export const Title = styled.h2`
   margin: 159px 86px 25px 147px;
-  font-family: Poppins,sans-serif;
+  font-family: Poppins;
   font-size: 36px;
   font-weight: bold;
   line-height: normal;
@@ -16,26 +16,20 @@ export const Title = styled.h2`
 export const Subtitle = styled.h3`
   width: 477px;
   margin: 25px 102px 25px 147px;
-  font-family: Poppins,sans-serif;
+  font-family: Poppins;
   font-size: 24px;
   font-weight: 500;
-  font-stretch: normal;
-  font-style: normal;
   line-height: 1.5;
-  letter-spacing: normal;
   text-align: left;
   color: #000000;
 `;
 export const Paragraph = styled.p`
   width: 477px;
   margin: 25px 105px 211px 146px;
-  font-family: Poppins,sans-serif;
+  font-family: Poppins;
   font-size: 14px;
   font-weight: normal;
-  font-stretch: normal;
-  font-style: normal;
   line-height: 2.57;
-  letter-spacing: normal;
   text-align: left;
   color: #000000;
 `;
