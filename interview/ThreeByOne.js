68c9e8479e161df3bf1d7bcc88faf4a3907bd029
Zhang-Haoran <haoranzhang001@gmail.com>
2022-04-05 14:26:08 +1000

    updated

--- interview/ThreeByOne.js
+++ interview/ThreeByOne.js
@@ -1,4 +1,5 @@
 /**
+ * This interview will focus mainly on more technical aspects of the role as provide some more context of the position itself and within the business.
  * Brief:
  * global denim business
  * engage customer through the brand and enhance their social confidence
@@ -45,4 +46,23 @@
  * JS, CSS, Jquery
  * Sketch, adobe suite, Invision and AB testing platforms
  * customer journey mapping, AB testing
+ *
+ * Prepare:
+ * AB testing: comparing two version of webpage to decide which is better. two version is slightly different.half of the traffic see the first version, half of the traffic see the second version. check the analytics tools.
+ * AB testing is used to make careful changes to user experience while collecting data on the result
+ * software: Google Optimize
+ * To increase number of checkouts, the average order value, increase holiday sales
+ * run AB testing on homepage promotion, navigation element, checkout components
+ *
+ * touchponint
+ * shopify plus provide headless commerce
+ * algolia: hosted search engine with searching data analytics
+ * arkade: improve customer experience
+ * Headless commerce separates your back-end infrastructure from your front-end customer touchpoints
+ * Prismic
+ * Next.js
+ * 这是一个超级复杂的树状结构，如果我们用常见的 RESTful API 涉及，
+ * 每个 API 负责请求一种类型的对象，例如用户是一个类型，帖子是另一个类型，那就需要非常多个请求才能把这个页面所需的所有数据拿回来。
+ * 而且这些请求直接还存在依赖关系，不能平行地发多个请求
+ * 数据已经以图的数据结构进行保存
  */
