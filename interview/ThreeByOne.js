5cdde75d3ede4b190d5299d7217de320adcd059a
Zhang-Haoran <haoranzhang001@gmail.com>
2022-04-05 10:24:34 +1000

    updated

---
+++ interview/ThreeByOne.js
@@ -0,0 +1,48 @@
+/**
+ * Brief:
+ * global denim business
+ * engage customer through the brand and enhance their social confidence
+ * most desirable denim brand in the world.
+ * integral role with digital and eCommerce business
+ * frontend customer experience, landing page design, optimisation, development of Prismic CMS
+ * eCommerce AB testing
+ * transfer elevate customer experience
+ * report to genernal manager of digital and customer experience
+ *
+ * UX/UI:
+ * manage prismic CMS, create templates
+ * work with eCommerce to create unique homepage and landing page design
+ * website usability, accessibility
+ * error message
+ * maintain design system and UI kits
+ * ensure mobile responsive
+ *
+ * Frontend:
+ * AB testing strategy
+ * ongoing website optimization
+ * SEO
+ * ensure code standards
+ *
+ * Backend:
+ * integrate with third party
+ * create back end processes to free up resources
+ * managing API with third party
+ * cloud architecting system
+ *
+ * Skill:
+ * HTML, CSS, JS
+ * React,
+ * next.js
+ * RESTAPI
+ * Shopify
+ * GraphQL
+ * Node.js
+ * frontend from conception to deployment
+ * focusing on Ecommerce and UX
+ * SEO
+ * Analytical reporting tools
+ * integrate with other digital operations system
+ * JS, CSS, Jquery
+ * Sketch, adobe suite, Invision and AB testing platforms
+ * customer journey mapping, AB testing
+ */
